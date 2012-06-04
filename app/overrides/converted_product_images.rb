ade637465fe09f8a3096151d172b9ccc3ba08278
Aubrey <aubrey.goodman@gmail.com>
2012-06-03 23:07:17 -0400

    fixed namespaces on virtual paths

--- app/overrides/converted_product_images.rb
+++ app/overrides/converted_product_images.rb
@@ -1,14 +1,14 @@
-Deface::Override.new(:virtual_path => "products/show",
+Deface::Override.new(:virtual_path => "spree/products/show",
                      :name => "converted_product_images_30349746",
                      :replace => "[data-hook='product_images'], #product_images[data-hook]",
                      :partial => "products/product_images",
                      :disabled => false)
-Deface::Override.new(:virtual_path => "products/show",
+Deface::Override.new(:virtual_path => "spree/products/show",
                      :name => "converted_product_images_247578002",
                      :insert_after => "[data-hook='product_images'], #product_images[data-hook]",
                      :partial => "shared/script_for_facebox",
                      :disabled => false)
-Deface::Override.new(:virtual_path => "layouts/spree_application",
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "converted_inside_head_611705778",
                      :insert_after => "[data-hook='inside_head'], #inside_head[data-hook]",
                      :partial => "shared/facebox_include",
