e304a47f4ecb6bb4ff72828fab55e9dfdd7c37f4
Aubrey Goodman <aubrey.goodman@gmail.com>
2012-06-03 23:33:06 -0300

    added spree namespace to model references in activate

--- lib/spree_facebox.rb
+++ lib/spree_facebox.rb
@@ -12,17 +12,17 @@ module SpreeFacebox
       end
 
       #Add an extra large (xl) size to use for zooming.
-      Image.attachment_definitions[:attachment][:styles] =
+      Spree::Image.attachment_definitions[:attachment][:styles] =
         { :mini => '48x48>', :small => '100x100>', :product => '240x240>',
           :large => '600x600>', :xl => '900x900>' }
 
-      ProductsHelper.class_eval do
+      Spree::ProductsHelper.class_eval do
         def image_controls
           @image_controls ||= Facebox::ImageControls.new(@product)
         end
       end
 
-      Product.class_eval do
+      Spree::Product.class_eval do
         def has_image_without_style?(style)
           return true if contains_image_without_style?(images, style)
           if !variants.blank?
