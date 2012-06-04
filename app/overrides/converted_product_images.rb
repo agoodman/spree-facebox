c3a666c17ee2499c21bf4fa8deee2aa9f8403fcb
Aubrey <aubrey.goodman@gmail.com>
2012-06-03 23:42:29 -0400

    minor adjustments to markup and script trigger loading

--- app/overrides/converted_product_images.rb
+++ app/overrides/converted_product_images.rb
@@ -1,15 +1,10 @@
 Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "converted_product_images_30349746",
+                     :name => "product_images",
                      :replace => "[data-hook='product_images'], #product_images[data-hook]",
                      :partial => "spree/products/product_images",
                      :disabled => false)
 Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "converted_product_images_247578002",
+                     :name => "facebox_trigger_script",
                      :insert_after => "[data-hook='product_images'], #product_images[data-hook]",
                      :partial => "spree/shared/script_for_facebox",
-                     :disabled => false)
-Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "converted_inside_head_611705778",
-                     :insert_after => "[data-hook='inside_head'], #inside_head[data-hook]",
-                     :partial => "spree/shared/facebox_include",
                      :disabled => false)
